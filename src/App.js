7ecfa234e33d4a10dbef51db5f68c911f56be6f0
Swaranjana Nayak <58822437+sn2606@users.noreply.github.com>
2021-05-12 19:11:52 +0530

    Module 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>React App</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello. I\'m a react app'));
+  }
 }
 
 export default App;
